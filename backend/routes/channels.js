6f35216b08122370ceb735c3945e761636a4125b
Benjamin Frost <bfrost.schule@gmail.com>
2020-08-27 17:12:33 +0200

    Add Backend Channel Edit

--- backend/routes/channels.js
+++ backend/routes/channels.js
@@ -95,7 +95,7 @@ router.post('/create', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     // Check if passed id is a valid mongoDB ID
-    if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
         return res.status(400).send('The passed ID is invalid')
     }
 
@@ -117,7 +117,7 @@ router.delete('/:id', async (req, res) => {
         return res.status(404).send('Could not find a channel with the requested ID')
     }
 
-    // Make sure the delete request was made by the channel owner
+    // Make sure the request was made by the channel owner
     if (channel.owner_uid != req.tsUid) {
         return res.status(403).send('You cannot delete a channel that does not belong to you.')
     }
@@ -127,17 +127,88 @@ router.delete('/:id', async (req, res) => {
         const tsDelete = ts3Query.channelDelete(channel.channel_id, true)
 
         // Delete Database Entry
-        const dbDelete = Channel.findByIdAndDelete(channel._id)
+        const dbDelete = channel.deleteOne()
 
-        // Execute both at the same time
+        // We called both in parallel (no 'await') => Therefore wait until both have finished
         await Promise.all([tsDelete, dbDelete])
     }
-    catch(err) {
+    catch (err) {
         console.log(err)
         return res.status(500).send('An error occured when trying to delete channel')
     }
 
     res.send('Channel was deleted successfully')
+})
+
+router.patch('/:id', async (req, res) => {
+    // Check if a new channel name and/or password was requested
+    const cName = req.body.channelName
+    const cPassword = req.body.channelPassword
+
+    if (!cName && !cPassword)
+        return res.status(400).send('Please provide a new channel name and/or a new password')
+
+    // Check if passed id is a valid mongoDB ID
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).send('The passed ID is invalid')
+    }
+
+    // Get Owner UID and Channel ID from Database
+    let channel;
+    try {
+        channel = await Channel.findOne(
+            { _id: req.params.id },
+            'channel_id channel_num owner_uid'
+        ).exec()
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(500).send('An error occured when trying to access database')
+    }
+
+    // Check if a channel with that ID exists
+    if (!channel) {
+        return res.status(404).send('Could not find a channel with the requested ID')
+    }
+
+    // Make sure the request was made by the channel owner
+    if (channel.owner_uid != req.tsUid) {
+        return res.status(403).send('You cannot edit a channel that does not belong to you.')
+    }
+
+    // Update channel information
+    let newChannelProps = {}
+
+    if (cPassword) {
+        newChannelProps["channelPassword"] = cPassword
+    }
+
+    if (cName) {
+        newChannelProps["channelName"] = `[${channel.channel_num}] ${cName}`
+    }
+
+    try {
+        let patchPromises = []
+        // Update Channel on TS3 Server
+        const tsPatch = ts3Query.channelEdit(channel.channel_id, newChannelProps)
+        patchPromises.push(tsPatch)
+
+        // Update Channel in Database
+        if (cName) {
+            channel.channel_name = cName
+            const dbPatch = channel.save()
+            patchPromises.push(dbPatch)
+        }
+
+        // We called both in parallel (no 'await') => Therefore wait until both have finished
+        await Promise.all(patchPromises)
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(500).send('An error occured when trying to patch channel')
+    }
+
+    res.send('Channel was updated successfully')
 })
 
 module.exports = router;
